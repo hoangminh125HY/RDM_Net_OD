6407a3f9c29fac4bc440dfb69579116e93308dff
Apple <apple@MacBook-Pro-cua-Minh.local>
2025-07-24 01:14:38 +0700

    Initial commit

--- nets/moco.py
+++ nets/moco.py
@@ -2,7 +2,6 @@
 import torch
 import torch.nn as nn
 
-
 class MoCo(nn.Module):
     """
     Build a MoCo model with: a query encoder, a key encoder, and a queue
@@ -20,15 +19,15 @@ class MoCo(nn.Module):
         self.K = K
         self.m = m
         self.T = T
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
         # create the encoders
-        # num_classes is the output fc dimension
         self.encoder_q = base_encoder()
         self.encoder_k = base_encoder()
 
         for param_q, param_k in zip(self.encoder_q.parameters(), self.encoder_k.parameters()):
             param_k.data.copy_(param_q.data)  # initialize
-            param_k.requires_grad = False  # not update by gradient
+            param_k.requires_grad = False     # not update by gradient
 
         # create the queue
         self.register_buffer("queue", torch.randn(dim, K))
@@ -38,50 +37,30 @@ class MoCo(nn.Module):
 
     @torch.no_grad()
     def _momentum_update_key_encoder(self):
-        """
-        Momentum update of the key encoder
-        """
         for param_q, param_k in zip(self.encoder_q.parameters(), self.encoder_k.parameters()):
             param_k.data = param_k.data * self.m + param_q.data * (1. - self.m)
 
     @torch.no_grad()
     def _dequeue_and_enqueue(self, keys):
-        # gather keys before updating queue
-        # keys = concat_all_gather(keys)
         batch_size = keys.shape[0]
-
         ptr = int(self.queue_ptr)
         assert self.K % batch_size == 0  # for simplicity
 
-        # replace the keys at ptr (dequeue and enqueue)
-        self.queue[:, ptr:ptr + batch_size] = keys.transpose(0, 1)
-        ptr = (ptr + batch_size) % self.K  # move pointer
-
+        self.queue[:, ptr:ptr + batch_size] = keys.T
+        ptr = (ptr + batch_size) % self.K
         self.queue_ptr[0] = ptr
 
     @torch.no_grad()
     def _batch_shuffle_ddp(self, x):
-        """
-        Batch shuffle, for making use of BatchNorm.
-        *** Only support DistributedDataParallel (DDP) model. ***
-        """
-        # gather from all gpus
         batch_size_this = x.shape[0]
         x_gather = concat_all_gather(x)
         batch_size_all = x_gather.shape[0]
-
         num_gpus = batch_size_all // batch_size_this
 
-        # random shuffle index
-        idx_shuffle = torch.randperm(batch_size_all).cuda()
-
-        # broadcast to all gpus
+        idx_shuffle = torch.randperm(batch_size_all, device=x.device)
         torch.distributed.broadcast(idx_shuffle, src=0)
 
-        # index for restoring
         idx_unshuffle = torch.argsort(idx_shuffle)
-
-        # shuffled index for this gpu
         gpu_idx = torch.distributed.get_rank()
         idx_this = idx_shuffle.view(num_gpus, -1)[gpu_idx]
 
@@ -89,80 +68,56 @@ class MoCo(nn.Module):
 
     @torch.no_grad()
     def _batch_unshuffle_ddp(self, x, idx_unshuffle):
-        """
-        Undo batch shuffle.
-        *** Only support DistributedDataParallel (DDP) model. ***
-        """
-        # gather from all gpus
         batch_size_this = x.shape[0]
         x_gather = concat_all_gather(x)
         batch_size_all = x_gather.shape[0]
-
         num_gpus = batch_size_all // batch_size_this
 
-        # restored index for this gpu
         gpu_idx = torch.distributed.get_rank()
         idx_this = idx_unshuffle.view(num_gpus, -1)[gpu_idx]
 
         return x_gather[idx_this]
 
     def forward(self, im_q, im_k=None):
-        """
-        Input:
-            im_q: a batch of query images
-            im_k: a batch of key images
-        Output:
-            logits, targets
-        """
         if self.training:
             # compute query features
-            q, inter = self.encoder_q(im_q)  # queries: NxC
+            q, inter = self.encoder_q(im_q)
             q = nn.functional.normalize(q, dim=1)
 
             # compute key features
-            with torch.no_grad():  # no gradient to keys
-                self._momentum_update_key_encoder()  # update the key encoder
-
-                k, _ = self.encoder_k(im_k)  # keys: NxC
+            with torch.no_grad():
+                self._momentum_update_key_encoder()
+                k, _ = self.encoder_k(im_k)
                 k = nn.functional.normalize(k, dim=1)
 
-            # compute logits
-            # Einstein sum is more intuitive
-            # positive logits: Nx1
-            # q和k为几种不同降质图，此操作为对应向量相乘，即degrade_patch1 x degrade_patch2
+            # positive logits
             l_pos = torch.einsum('nc,nc->n', [q, k]).unsqueeze(-1)
-            # negative logits: NxK
+            # negative logits
             l_neg = torch.einsum('nc,ck->nk', [q, self.queue.clone().detach()])
-
             # logits: Nx(1+K)
             logits = torch.cat([l_pos, l_neg], dim=1)
-
-            # apply temperature
             logits /= self.T
 
             # labels: positive key indicators
-            labels = torch.zeros(logits.shape[0], dtype=torch.long).cuda()
+            labels = torch.zeros(logits.shape[0], dtype=torch.long, device=logits.device)
 
-            # dequeue and enqueue
             self._dequeue_and_enqueue(k)
-
             return logits, labels, inter
         else:
             _, inter = self.encoder_q(im_q)
-
             return inter
 
 
-# utils
 @torch.no_grad()
 def concat_all_gather(tensor):
     """
     Performs all_gather operation on the provided tensors.
     *** Warning ***: torch.distributed.all_gather has no gradient.
     """
-    tensors_gather = [torch.ones_like(tensor)
-        for _ in range(torch.distributed.get_world_size())]
+    tensors_gather = [
+        torch.ones_like(tensor)
+        for _ in range(torch.distributed.get_world_size())
+    ]
     torch.distributed.all_gather(tensors_gather, tensor, async_op=False)
-
     output = torch.cat(tensors_gather, dim=0)
     return output
