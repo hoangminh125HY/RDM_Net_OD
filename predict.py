4f37f4c7b9c00dae28df7e898282ea806172d00d
Apple <apple@MacBook-Pro-cua-Minh.local>
2025-07-27 13:09:38 +0700

    Initial commit

--- predict.py
+++ predict.py
@@ -25,8 +25,8 @@ if __name__ == "__main__":
 
     test_interval = 100
 
-    dir_origin_path = r"img"
-    dir_save_path = r"img_out"
+    dir_origin_path = r"/kaggle/working/VOC_Split/test/images"
+    dir_save_path = r"/kaggle/working/VOC_Split/test"
 
     if mode == "predict":
 
