1b380fede4ba097739144a92bb822d4486a8b1f2
Apple <apple@MacBook-Pro-cua-Minh.local>
2025-08-07 10:20:37 +0700

    Initial commit

--- yolo.py
+++ yolo.py
@@ -15,8 +15,8 @@ from utils.utils_bbox import decode_outputs, non_max_suppression
 class YOLO(object):
     _defaults = {
 
-        "model_path": r'logs/ep025-loss1.546-val_loss1.622.pth',
-        "classes_path": 'model_data/rtts_classes.txt',
+        "model_path": r'/kaggle/working/logs/ep003-loss4.961-val_loss4.683.pth',
+        "classes_path": '/kaggle/working/RDM_Net_OD/model_data/rtts_classes.txt',
 
         "input_shape": [640, 640],
 
